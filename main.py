24da5a2878ca234bd70c36f8fe0a7889b2ed60a3
Rishi Vishwakarma <rishipi@proton.me>
2024-09-04 14:37:53 +0530

    final1

---
+++ main.py
@@ -0,0 +1,145 @@
+from datetime import datetime
+from time import sleep
+from dateutil.relativedelta import relativedelta
+import requests
+
+from selenium.webdriver import Edge
+from selenium.webdriver.edge.options import Options
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+
+import os
+from bs4 import BeautifulSoup
+
+if 'data' not in os.listdir('.'):
+    os.mkdir('./data')
+os.chdir('./data')
+
+
+STATE_NO = 2
+YEAR_DIFF = 10
+
+def ue(s):
+    return requests.utils.quote(str(s))
+
+
+def dc(s):
+    d = datetime.strptime(s, r"%d %b %Y")
+    return d.strftime(r"%Y,%B,%d")
+
+
+# then = ue("{:%d-%b-%Y}".format(now - relativedelta(years=YEAR_DIFF)))
+then = "01-Jan-2014"
+now = ue("{:%d-%b-%Y}".format(datetime.now()))
+print(now)
+
+def marketgetter(markets):
+    b = dict()
+    a = [i.split('">') for i in markets.get_attribute('innerHTML').split('</option>')]
+    for i in a:
+        if len(i) > 1:
+            val =  i[0].split('value="')[1]
+            if val != '0':
+                b[i[1]] = val
+    return b
+
+comms = {
+    "Food Grains": {
+        "Rice": 3,
+        "Wheat": 1,
+        "Wheat Atta": 287,
+        "Maida": 288
+    },
+    "Pulses": {
+        "Gram": 263, "Tur": 260, "Urad": 264, "Moong": 265, "Masur": 259
+    },
+    "Edible Oils": {
+        "Groundnut oil": 267,
+        "Mustard Oil": 324,
+        "Vanaspati": 273,
+        # "Soya oil",
+        # "Sunflower oil",
+        # "Palm oil"
+    },
+    "Vegetables": {
+        "Potato": 24,
+        "Onion": 23,
+        "Tomato": 78
+    },
+    "Others": {
+        "Sugar": 48,
+        "Gur": 74,
+        # "Milk",
+        "Tea": 44,
+        # "Salt"
+    }
+}
+
+
+opts = Options()
+browser = Edge(options=opts)
+browser.get('https://agmarknet.gov.in/')
+sleep(2)
+
+states = Select(browser.find_element(by=By.ID, value='ddlState'))
+stname = states.options[STATE_NO].text
+stcode = states.options[STATE_NO].get_attribute('value')
+states.select_by_index(STATE_NO)
+
+sleep(1)
+
+districts = Select(browser.find_element(by=By.ID, value='ddlDistrict'))
+for i in range(1, len(districts.options)):
+    distname = districts.options[i].text
+    distcode = districts.options[i].get_attribute('value')
+    districts.select_by_index(i)
+    sleep(0.1)
+    
+    districts = Select(browser.find_element(by=By.ID, value='ddlDistrict'))
+    markets = browser.find_element(by=By.ID, value='ddlMarket')
+
+    for k,v in marketgetter(markets).items():
+        mrname = k
+        mrcode = v
+        for category in comms.keys():
+            for commodity in comms[category].keys():
+                url = rf'https://agmarknet.gov.in/SearchCmmMkt.aspx?Tx_Commodity={ue(comms[category][commodity])}&Tx_State={ue(stcode)}&Tx_District={ue(distcode)}&Tx_Market={ue(mrcode)}&DateFrom={then}&DateTo={now}&Fr_Date={then}&To_Date={now}&Tx_Trend=2&Tx_CommodityHead={ue(commodity)}&Tx_StateHead={ue(stname)}&Tx_DistrictHead={ue(distname)}&Tx_MarketHead={ue(mrname)}'
+                req = requests.get(url)
+                reqdata = BeautifulSoup(req.text, 'html.parser')
+                reslabel = reqdata.find(id="cphBody_Label_Result")
+                if " Not Available" in reslabel.text:
+                    print(f'NOT FOUND {stname} > {distname} > {mrname} > {commodity}')
+                    continue
+                elif not reslabel.text:
+                    table = reqdata.find(id="cphBody_GridViewBoth")
+                    filedata = 'year,month,date,arrival,min,max,modal\n'
+                    for row in tuple(table.children)[2:-3]:
+                        cols = tuple(row.children)
+                        try:
+                            ymd = dc(cols[10].text)
+                            filedata += f'{ymd},{cols[6].text},{cols[7].text},{cols[8].text},{cols[9].text}\n'
+                        except ValueError:
+                            continue
+
+                    if stname not in os.listdir('.'):
+                        os.mkdir(stname)
+                    os.chdir(stname)
+                    if distname not in os.listdir('.'):
+                        os.mkdir(distname)
+                    os.chdir(distname)
+                    if mrname not in os.listdir('.'):
+                        os.mkdir(mrname)
+                    os.chdir(mrname)
+                    if category not in os.listdir('.'):
+                        os.mkdir(category)
+                    os.chdir(category)
+                    if commodity not in os.listdir('.'):
+                        os.mkdir(commodity)
+                    os.chdir(commodity)
+
+                    with open('data.csv', 'w') as f:
+                        f.write(filedata)
+                        print(f'DATA WRITTEN {stname} > {distname} > {mrname} > {commodity}')
+                    os.chdir('../../../../..')
+                        
+sleep(2)
---
+++ readme.md
@@ -0,0 +1,32 @@
+### step 1:
+clone this repository
+
+### step 2:
+create a virtual environment by 
+`python -m venv venv`
+
+### step 3:
+activate the virtual environment by 
+`source ./venv/Scripts/activate` \
+this works in bash dont know for windows : )
+
+### step 4:
+install dependencies by 
+`pip install -r requirements.txt`
+
+### step 5:
+run `main.py` after change line 19: `STATE_NO = 1` to different numbers one by one
+
+![](./image.png)
+
+state list is like this with `STATE_NO = 1` referring to `Andaman and Nicobar`
+
+### Different members are expected to run the script for different states
+
+### script will open a browser window (just minimize that and focus on terminal) and most probably save the desired data
+
+ A foldare called `data` will be created. After fetching/writing is done is done for the particular state script will end.
+
+ #### Continue with the next state.
+
+
